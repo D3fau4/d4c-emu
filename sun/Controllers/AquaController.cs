502044884339fd8dd03895fb50f97b1b7f3aee19
Raúl Álvarez <raulwii2@gmail.com>
2022-04-18 11:09:32 +0200

    Setup Aqua

---
+++ sun/Controllers/AquaController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace sun.Controllers;
+
+[ApiController]
+[Produces("application/json")]
+public class AquaController : ControllerBase
+{
+    [HttpGet]
+    [Route("required_system_update_meta")]
+    public ActionResult<string> GetLastestMandatoryUpdate(string device_id = "DEADCAFEBABEBEEF")
+    {
+        if (device_id.Length < 16)
+            return  StatusCode(StatusCodes.Status403Forbidden);
+        return Ok($"Hello from aqua");
+    }
+}
