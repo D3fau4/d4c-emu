2dc02f27a819d3e9ab9d4c09ff2629191678260a
Raúl Álvarez <raulwii2@gmail.com>
2022-04-18 11:04:58 +0200

    Setup sun

---
+++ sun/Controllers/SunController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace sun.Controllers;
+
+[ApiController]
+[Route("v1/")]
+[Produces("application/json")]
+public class SunController : ControllerBase
+{
+    [HttpGet]
+    [Route("system_update_meta")]
+    public ActionResult<string> GetLastestUpdate(string device_id = "DEADCAFEBABEBEEF")
+    {
+        if (device_id.Length < 16)
+            return  StatusCode(StatusCodes.Status403Forbidden);
+        return Ok($"{device_id}");
+    }
+}
