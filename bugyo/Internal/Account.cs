7370a4c2367a85bced57782dc4b152432f8cc81d
Raúl Álvarez <raulwii2@gmail.com>
2022-11-21 10:33:57 +0100

    Update libs

--- bugyo/Internal/Account.cs
+++ bugyo/Internal/Account.cs
@@ -3,15 +3,15 @@
 public class Account
 {
     public ulong shop_account_id { get; set; }
-    public string na_id { get; set; }
-    public string screen_name { get; set; }
-    public string gender { get; set; }
-    public string country { get; set; }
-    public string language { get; set; }
-    public string timezone { get; set; }
-    public string birthday { get; set; }
+    public string? na_id { get; set; }
+    public string? screen_name { get; set; }
+    public string? gender { get; set; }
+    public string? country { get; set; }
+    public string? language { get; set; }
+    public string? timezone { get; set; }
+    public string? birthday { get; set; }
     public int age { get; set; }
-    public string payment { get; set; }
+    public string? payment { get; set; }
     public bool analytics_opted_in { get; set; }
     public bool shop_analytics_opted_in { get; set; }
     public bool ecash_integrated { get; set; }
